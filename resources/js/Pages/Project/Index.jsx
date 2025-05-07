d6bd34bfe855d182673ab1cc443051043b8ce31c
Nikolajs Aleksejenkovs <nikolaj.aleksejenkov@gmail.com>
2025-05-07 19:43:35 +0300

    feat:  added projects table - implemented a table for showing project data + action buttons in frontend - created reusable Pagination component to work with the table - added constants.js to store status text and classes - added a line in tailwind.config so it would watch also .js files and apply classes - tweaked database seeder to generate 30 tasks and projects, to help visualise pagination better

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,7 @@
+import Pagination from "@/Components/Pagination";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ projects }) {
     return (
@@ -16,7 +18,94 @@ export default function Index({ projects }) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <pre>{JSON.stringify(projects, undefined, 2)}</pre>
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="px-3 py-2">ID</th>
+                                        <th className="px-3 py-2">Image</th>
+                                        <th className="px-3 py-2">Name</th>
+                                        <th className="px-3 py-2">Status</th>
+                                        <th className="px-3 py-2">
+                                            Create Date
+                                        </th>
+                                        <th className="px-3 py-2">Due Date</th>
+                                        <th className="px-3 py-2">
+                                            Created By
+                                        </th>
+                                        <th className="px-3 py-2 text-right">
+                                            Actions
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {projects.data.map((project) => (
+                                        <tr
+                                            key={project.id}
+                                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                        >
+                                            <td className="px-3 py-2">
+                                                {project.id}
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                <img
+                                                    src={project.image_path}
+                                                    alt=""
+                                                    style={{ width: 60 }}
+                                                />
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                {project.name}
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                <span
+                                                    className={
+                                                        "px-2 py-1 rounded text-white " +
+                                                        PROJECT_STATUS_CLASS_MAP[
+                                                            project.status
+                                                        ]
+                                                    }
+                                                >
+                                                    {
+                                                        PROJECT_STATUS_TEXT_MAP[
+                                                            project.status
+                                                        ]
+                                                    }
+                                                </span>
+                                            </td>
+                                            <td className="px-3 py-2 text-nowrap">
+                                                {project.created_at}
+                                            </td>
+                                            <td className="px-3 py-2 text-nowrap">
+                                                {project.due_date}
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                {project.createdBy.name}
+                                            </td>
+                                            <td className="px-3 py-2">
+                                                <Link
+                                                    href={route(
+                                                        "project.edit",
+                                                        project.id
+                                                    )}
+                                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                                >
+                                                    Edit
+                                                </Link>
+                                                <Link
+                                                    href={route(
+                                                        "project.destroy",
+                                                        project.id
+                                                    )}
+                                                    className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                                >
+                                                    Delete
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                            <Pagination links={projects.meta.links} />
                         </div>
                     </div>
                 </div>
